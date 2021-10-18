b8208e9e763f0ff3ede28b5864ccdda5d6b53d35
The3Legend <durdvlad@gmail.com>
2021-10-18 19:50:19 +0300

    start project

---
+++ src/components/header.vue
@@ -0,0 +1,39 @@
+<template>
+  <div>
+    <b-navbar toggleable="lg" type="light" variant="light ">
+      <b-navbar-brand href="#">Meclee Test</b-navbar-brand>
+
+      <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+
+      <b-collapse id="nav-collapse" is-nav>
+        <b-navbar-nav>
+          <b-nav-item
+            v-for="categorie in categories"
+            :key="categorie"
+            href="#"
+            >{{ categorie }}</b-nav-item
+          >
+        </b-navbar-nav>
+      </b-collapse>
+      <b-button size="md" variant="secondary " class="mb-2">
+        <b-icon icon="cart3" aria-label="Pay"></b-icon>
+      </b-button>
+    </b-navbar>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Header",
+  data: () => ({
+    categories: [],
+  }),
+  created() {
+    fetch("https://fakestoreapi.com/products/categories")
+      .then((res) => res.json())
+      .then((json) => (this.categories = json));
+  },
+};
+</script>
+
+<style></style>
